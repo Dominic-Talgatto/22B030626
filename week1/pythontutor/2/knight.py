fb849fd7b49284f6f43fe16ac8811b825ab4c492
Dominic-Talgatto <110767895+Dominic-Talgatto@users.noreply.github.com>
2023-01-29 02:30:03 +0600

    Create knight.py

---
+++ week1/pythontutor/2/knight.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if abs(a-c) == 2:
+    if abs(b-d) == 1:
+        print("YES")
+    else: print("NO")
+elif abs(b-d) == 2:
+    if abs(a-c) == 1:
+        print("YES")
+    else:
+        print("NO")
+else:
+    print("NO")
