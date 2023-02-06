005931975b3fed8651bb1c49ff09334e22472be2
Dominic-Talgatto <110767895+Dominic-Talgatto@users.noreply.github.com>
2023-02-07 00:22:22 +0600

    Update 1.py

--- week2/bonus_task/str/1.py
+++ week2/bonus_task/str/1.py
@@ -1,26 +1,10 @@
 s = input()
-b = ""
-a = ""
 print(s[2])
 print(s[-2])
 print(s[:5])
 print(s[:-2])
-for i in range(len(s)):
-    if i % 2 == 0:
-        a = a + s[i]
-    else:
-        b += s[i]
-print(a)
-print(b)
-arr = list(s)
-arr.reverse()
-s = ''
-a = ''
-for i in range(len(arr)):
-    s += arr[i]
-    if i % 2 == 0:
-        a += arr[i]
-print(s)
-print(a)
+print(s[::2])
+print(s[1::2])
+print(s[::-1])
+print(s[::-2])
 print(len(s))
-
